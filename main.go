39745013b47c6ab9e2a0774137aaf97830610764
Rodrigo D Baraglia Di Fulvio <ext_rdbaraglia@falabella.cl>
2021-01-21 15:54:50 -0300

    finished chapter 1, should complete tests

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"rodrigobaraglia/monkeyimpl/repl"
+)
 
 func main() {
-	fmt.Println("Hello  work in progress")
+
+	fmt.Println("Wait...")
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! Welcome to the MonkeyLang REPL!\n", user.Username)
+	fmt.Println("Type any command and see what happens")
+	repl.Start(os.Stdin, os.Stdout)
+
 }
--- token/token.go
+++ token/token.go
@@ -16,6 +16,12 @@ const (
 	Int       = "Int"
 	Assign    = "="
 	Plus      = "+"
+	Minus     = "-"
+	Bang      = "!"
+	Asterisk  = "*"
+	Slash     = "/"
+	LT        = "<"
+	GT        = ">"
 	Comma     = ","
 	Semicolon = ";"
 	LParen    = "("
@@ -24,4 +30,31 @@ const (
 	RBrace    = "}"
 	Function  = "Function"
 	Let       = "Let"
+	True      = "True"
+	False     = "False"
+	If        = "If"
+	Else      = "Else"
+	Return    = "Return"
+	Eq        = "=="
+	NotEq     = "!="
 )
+
+var keywords = map[string]Type{
+	"fn":     Function,
+	"let":    Let,
+	"true":   True,
+	"false":  False,
+	"if":     If,
+	"else":   Else,
+	"return": Return,
+}
+
+/*LookupIdent takes a token literal and looks it up in the keyword map. If the token is not a keyword, it returns
+an Ident type tag, else, it returns the token associated with the given keyword.*/
+func LookupIdent(ident string) Type {
+	tok, ok := keywords[ident]
+	if !ok {
+		return Ident
+	}
+	return tok
+}
