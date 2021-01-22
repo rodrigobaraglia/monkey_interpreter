ac9cc27df1ed8b198850f7a187f337166dc45552
Rodrigo D Baraglia Di Fulvio <ext_rdbaraglia@falabella.cl>
2021-01-22 15:00:48 -0300

    parsing let statements

--- parser/parser.go
+++ parser/parser.go
@@ -6,14 +6,23 @@ import (
 	t "rodrigobaraglia/monkeyimpl/token"
 )
 
+/*Parser is composed of a lexer l and two tokens curToken and peekToken.
+The first is the current token being parsed, the other is the next token to be parsed.*/
 type Parser struct {
 	l         *lx.Lexer
 	curToken  t.Token
 	peekToken t.Token
 }
 
+/*NewParser creates and initializes a Parser. It takes a lexer l as
+an argument to initialize the field by the same name and calls nextToken()
+twice to initialize fields curToken and peekToken.*/
 func NewParser(l *lx.Lexer) *Parser {
 	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+	return p
 }
 
 func (p *Parser) nextToken() {
@@ -21,6 +30,68 @@ func (p *Parser) nextToken() {
 	p.peekToken = p.l.NextToken()
 }
 
+/*ParseProgram returns the abstract syntax tree of a program*/
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curTokenIs(t.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case t.Let:
+		return p.parseLetStatement()
+
+	default:
+		return nil
+	}
+}
+
+/*parseLetStatement checks that the following context of a let statement corresponds to the proper form of assignement syntax and ends with a semicolon*/
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	/*Check that next token is an identifier*/
+	if !p.expectPeek(t.Ident) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	/*Check that next token is an assignement operator*/
+	if !p.expectPeek(t.Assign) {
+		return nil
+	}
+
+	/*Skip expressions until a semicolon is found*/
+	for !p.curTokenIs(t.Semicolon) {
+		p.nextToken()
+	}
+
+	return stmt
+
+}
+
+func (p *Parser) curTokenIs(tokType t.Type) bool {
+	return p.curToken.Type == tokType
+}
+
+func (p *Parser) peekTokenIs(tokType t.Type) bool {
+	return p.peekToken.Type == tokType
+}
+
+func (p *Parser) expectPeek(tokType t.Type) bool {
+	if !p.peekTokenIs(tokType) {
+		return false
+	}
+	p.nextToken()
+	return true
 }
--- token/token.go
+++ token/token.go
@@ -10,33 +10,60 @@ type Token struct {
 }
 
 const (
-	Illegal   = "Illegal"
-	EOF       = "EOF"
-	Ident     = "Ident"
-	Int       = "Int"
-	Assign    = "="
-	Plus      = "+"
-	Minus     = "-"
-	Bang      = "!"
-	Asterisk  = "*"
-	Slash     = "/"
-	LT        = "<"
-	GT        = ">"
-	Comma     = ","
-	Semicolon = ";"
-	LParen    = "("
-	RParen    = ")"
-	LBrace    = "{"
-	RBrace    = "}"
-	Function  = "Function"
-	Let       = "Let"
-	True      = "True"
-	False     = "False"
-	If        = "If"
-	Else      = "Else"
-	Return    = "Return"
-	Eq        = "=="
-	NotEq     = "!="
+	//Illegal tags an illegal token
+	Illegal Type = "Illegal"
+	//EOF tags the end of the file or input
+	EOF Type = "EOF"
+	//Ident tags an identifier token
+	Ident Type = "Ident"
+	//Int tags an ingeger
+	Int Type = "Int"
+	//Assign tags an assignement operator token
+	Assign Type = "="
+	//Plus tags a plus operator token
+	Plus Type = "+"
+	//Minus tags a minus operator token
+	Minus Type = "-"
+	//Bang tags a negation operator token
+	Bang Type = "!"
+	//Asterisk tags a multipication operator token
+	Asterisk Type = "*"
+	//Slash tags a division operator token
+	Slash Type = "/"
+	//LT tags a "lesser than" operator token
+	LT Type = "<"
+	//GT tags a "greater than" operator token
+	GT Type = ">"
+	//Comma tags a comma token
+	Comma Type = ","
+	//Semicolon tags a semicolon token
+	Semicolon Type = ";"
+	//LParen tags an opening parentheses token
+	LParen Type = "("
+	//RParen tags a closing parentheses token
+	RParen Type = ")"
+	//LBrace tags an opening brace token
+	LBrace Type = "{"
+	//RBrace tags a closing brace token
+	RBrace Type = "}"
+	//Function tags a function definition keyword token
+	Function Type = "Function"
+	//Let tags a variable definition keyword token
+	Let Type = "Let"
+	//True tags a boolean 1 token
+	True Type = "True"
+	//False tags a boolean 0 token
+	False Type = "False"
+	//If tags a conditional main branch keyword token
+	If Type = "If"
+	//Else tags a conditional alternate branch keyword token
+	Else Type = "Else"
+	//Return tags a return value token
+	Return Type = "Return"
+	//Eq tags an equality boolean operator token
+	Eq Type = "=="
+	//NotEq tags an inequality boolean operator token
+	NotEq Type = "!="
 )
 
 var keywords = map[string]Type{
